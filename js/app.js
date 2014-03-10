72667036749e5627bc0fbc6771d5994937fe16f4
bbogar <blaine.bogar@gmail.com>
2014-03-09 23:59:15 -0600

    4th Commit - CSS Setup cont.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+//gets the value from the input
+//should go inside event listner when 'enter' or button is clicked
+var item=$("input[name=checkListItem]").val();
 
 
 });
