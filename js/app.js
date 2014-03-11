82a6aaeeda100a31f8c6630159f9225baa612460
bbogar <blaine.bogar@gmail.com>
2014-03-11 09:28:05 -0600

    Tuesday Morning Commit - CSS menu + strikethrough

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 
 //gets the value from the input
 //should go inside event listner when 'enter' or button is clicked
-var item=$("input[name=checkListItem]").val();
+	$("#button").on("click", function(){
+		var item=$("input[name=checkListItem]").val();
+		$(".list-items").append("<div class='item'>" + item + "</div>");
+		$("form")[0].reset();	
+	});
 
+	$(document).on("click", ".item", function(){
+		$(this).addClass("strike");
+	});
 
+	// $("input").val("");
+	// $("form")[0].reset();
 });
