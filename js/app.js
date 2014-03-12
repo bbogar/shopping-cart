0143c5fd9672f58dde2892ac672b0b25ecc0dd27
bbogar <blaine.bogar@gmail.com>
2014-03-12 13:47:05 -0600

    Wed. 3/12

--- js/app.js
+++ js/app.js
@@ -1,17 +1,54 @@
+//////  Custom Functions  //////
+
+function addItem() {
+	if ($("input").val() == "") {
+		alert("Please enter an item");
+		return false;
+		$(".input").focus();
+	}
+	var item=$("input[name=checkListItem]").val();
+	$("#listItems").append("<li class='item'>" + 
+							item + 
+						   "<button class='edit'>EDIT</a>" + 
+				  		   "<button class='delete'>DELETE</a></li>");	
+	$("form")[0].reset();
+}
+
+function deleteItem() {
+	$(this).parent().remove();
+}
+
+
+
 $(document).ready(function(){
-
-//gets the value from the input
-//should go inside event listner when 'enter' or button is clicked
-	$("#button").on("click", function(){
-		var item=$("input[name=checkListItem]").val();
-		$(".list-items").append("<div class='item'>" + item + "</div>");
-		$("form")[0].reset();	
+	$(".input").focus();
+//gets the value when '+' is clicked
+	$("#button").click(addItem);
+//gets the value when 'enter' is pressed
+	$(".input").on("keydown", function(event){
+		if(event.keyCode == 13) {
+			addItem();
+		}
 	});
+	$(".input").focus();
 
+
+//////  Strikethrough and Remove Items  //////
+
+//adds strikethrough and fades list item when clicked
 	$(document).on("click", ".item", function(){
-		$(this).addClass("strike");
+		$(this).toggleClass("strike");
 	});
 
-	// $("input").val("");
-	// $("form")[0].reset();
+//Deletes item when 'delete' is clicked
+	$(".delete").on("click", function(){
+		console.log("Delete was clicked");
+	});
+
+//Edits item when 'edit' is clicked
+	$(".edit").on("click", function(){
+		console.log("Edit was clicked");
+	});
+
 });
+
