a7df18254f2f23cc611f0bfe6cd9972cfbf1f22d
bbogar <blaine.bogar@gmail.com>
2014-03-13 08:17:14 -0600

    Thurs. 3/13 - Fixed Delete, broke enter

--- js/app.js
+++ js/app.js
@@ -7,11 +7,10 @@ function addItem() {
 		$(".input").focus();
 	}
 	var item=$("input[name=checkListItem]").val();
-	$("#listItems").append("<li class='item'>" + 
-							item + 
-						   "<button class='edit'>EDIT</a>" + 
-				  		   "<button class='delete'>DELETE</a></li>");	
-	$("form")[0].reset();
+	$("#itemsList").append("<li class='item'>" + item + "</li>");
+	$(".item").parent().append("<div class='edit'>EDIT</div>" + 
+				  	"<div class='delete'>DELETE</div>");	
+	 $("form")[0].reset();
 }
 
 function deleteItem() {
@@ -27,28 +26,31 @@ $(document).ready(function(){
 //gets the value when 'enter' is pressed
 	$(".input").on("keydown", function(event){
 		if(event.keyCode == 13) {
-			addItem();
+		   addItem();
+		   //alert("enter was pressed");
 		}
 	});
 	$(".input").focus();
 
-
-//////  Strikethrough and Remove Items  //////
-
-//adds strikethrough and fades list item when clicked
-	$(document).on("click", ".item", function(){
-		$(this).toggleClass("strike");
-	});
-
-//Deletes item when 'delete' is clicked
-	$(".delete").on("click", function(){
+//Deletes item 
+	$(document).on("click", ".delete", function(){
+		$(this).prev().prev().remove();
+		$(this).prev().remove();
+		$(this).remove();
 		console.log("Delete was clicked");
 	});
 
-//Edits item when 'edit' is clicked
-	$(".edit").on("click", function(){
+//Edits item 
+	$(document).on("click", ".edit", function(){
 		console.log("Edit was clicked");
 	});
+
+//adds strikethrough and fades list item 
+	$(document).on("click", ".item", function(){
+		$(this).toggleClass("strike");
+		console.log("item was clicked");
+	});
+
 
 });
 
